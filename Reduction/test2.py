a6191e24b96d68632603830a43fd022056cf9a24
Hitesh Vaidya <hiteshv.tech@gmail.com>
2017-04-29 18:33:39 +0530

    Delete test2.py

--- Reduction/test2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import csv
-
-fname = input("Enter name of the weights file: ")
-with open('weights.csv') as csvfile:
-	readcsv = csv.reader(csvfile, delimiter=',')
-	layer = np.array(list(readcsv))
-	print(layer)
